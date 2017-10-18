cebf08d83868a9f60e1c8e49669f777f50cbff9b
rpathur <rpathur@ncsu.edu>
2017-10-18 12:07:33 -0400

    Update ConfigurationScript.sh

--- MS1-ConfigurationManagement/iTrust/ConfigurationScript.sh
+++ MS1-ConfigurationManagement/iTrust/ConfigurationScript.sh
@@ -1,1 +1,1 @@
-sudo ansible-playbook -i /home/vagrant/iTrust/inventory /home/vagrant/iTrust/iTrust.yml
+sudo ansible-playbook -i /home/vagrant/iTrust/inventory /home/vagrant/iTrust/iTrust.yml -e "githubuser=username" -e "githubpassword=password"
